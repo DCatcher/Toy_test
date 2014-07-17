d442473c9bd5dd720eb145a52b86e2f112c29563
zcx_win8 <nbfhzcx002@163.com>
2014-07-17 16:01:30 -0400

    Input parameters.

--- temp_Hiear/temp_supresol.m
+++ temp_Hiear/temp_supresol.m
@@ -1,4 +1,4 @@
-function temp_supresol(method)
+function temp_supresol(method, varargin)
 
 %addpath(genpath('d:\matlab2012a\GBMs\'));
 close all
@@ -6,6 +6,10 @@ rand('state', 0);
 randn('state', 0);
 
 pars    = pars_initial(method);
+pars    = parseArgs(varargin ,pars);
+
+disp(pars);
+pause;
 
 if strcmp(method, 'hiear')==1
 %% for hiear method
